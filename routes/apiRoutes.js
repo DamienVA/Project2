f4c1ad491b11ff6086efe26d538aa0c8cce4412c
Damien Aponte <damienapo@gmail.com>
2019-11-26 17:51:46 -0500

    minimum viable product

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,22 @@
 var db = require("../models");
 
-// const characters = [
-//   'Ryu.gif',
-// ]
+const characters = [
+  "Ryu.gif",
+  "Ken.gif",
+  "Chun-li.gif",
+  "Akuma.gif",
+  "M.Bison.gif",
+  "Guile.gif",
+  "Vega.gif",
+  "Dhalsim.gif"
+];
 
 module.exports = function(app) {
   // Get all todos
   app.get("/api/todos", async (req, res) => {
     try {
       const data = await db.Todo.findAll({});
+      // console.log(data.data.character);
       res.json(data);
     } catch (error) {
       res.status(400).json({ error: { name: error.name, msg: error.message } });
@@ -18,7 +26,18 @@ module.exports = function(app) {
   // Create a new todo
   app.post("/api/todos", async (req, res) => {
     try {
-      const result = await db.Todo.create(req.body);
+      const todoWithChars = req.body.map((todo, i) => {
+        return {
+          name: todo,
+          character: characters[i]
+        };
+      });
+      const result = await db.Todo.bulkCreate(todoWithChars);
+      // for (let i = 0; i < characters.length; i++) {
+      //   const result = await db.Todo.create({
+      //     name: req.body[i],
+      //     character: characters[i]
+      //   });
       res.json(result);
     } catch (error) {
       res.status(400).json({ error: { name: error.name, msg: error.message } });
